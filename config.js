177fe0c4ee70082db35f98a28fa5bf1c7c0c683d
nikoroxy145 <rtcocplay2@gmail.com>
2024-06-08 12:21:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "you are the cutest 😘",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "just look at my sweetest baby 🫂",  // 同上...
+        "your pout is the cutest thing ever 😘",
+        "you are my roundest baby 😘",
+        "and also my shawty 🫂",
+    ],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+    imgs: {
+        "you are the cutest 😘": "./imgs/message1.gif",
+        "just look at my sweetest baby 🫂": "./imgs/message2.jpeg",
+        "your pout is the cutest thing ever 😘": "./imgs/message3.jpeg",
+        "you are my roundest baby 😘": "./imgs/message4.jpeg",
+        "and also my shawty 🫂": "./imgs/message5.jpeg",
+    },
+
+    // 结束语
+    loveText: 'love you forever my Rashmi ♾️🍁❄️'
+};
